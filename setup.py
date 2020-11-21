ef8d9321c9ae3ba9da63d3937746965d1e403bef
davidmezzetti <561939+davidmezzetti@users.noreply.github.com>
2020-11-20 21:00:03 -0500

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
     DESCRIPTION = f.read()
 
 setup(name="tldrstory",
-      version="1.1.0",
+      version="1.2.0",
       author="NeuML",
       description="AI-powered understanding of headlines and story text",
       long_description=DESCRIPTION,
